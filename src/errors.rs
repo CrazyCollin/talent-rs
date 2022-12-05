d625ccddefea7e0deea3728286ea110cd32936d5
CrazyCollin <lazycollinl@gmail.com>
2022-12-05 21:58:27 +0800

    Refactor error handle

--- src/errors.rs
+++ src/errors.rs
@@ -3,57 +3,35 @@ use std::{io, result};
 use failure::Fail;
 use serde_json;
 use sled;
+use thiserror::Error;
 
 pub type Result<T>=result::Result<T,KvsError>;
 
-#[derive(Fail,Debug)]
+#[derive(Error,Debug)]
 pub enum KvsError{
-    #[fail(display = "{}", _0)]
-    Io(io::Error),
-    
-    #[fail(display="{}",_0)]
-    Serde(serde_json::Error),
-    
-    #[fail(display="{}",_0)]
-    Sled(sled::Error),
 
-    #[fail(display="{}",_0)]
-    FromUTF8Err(FromUtf8Error),
+    #[error("{0}")]
+    Io(#[from]io::Error),
+    
+    #[error("{0}")]
+    Serde(#[from]serde_json::Error),
+    
+    #[error("{0}")]
+    Sled(#[from]sled::Error),
 
-    #[fail(display="{}",_0)]
+    #[error("{0}")]
+    FromUTF8Err(#[from]FromUtf8Error),
+
+    #[error("{0}")]
     StrErr(String),
 
-    #[fail(display = "Key not found")]
+    #[error("Key not found")]
     KeyNotFound,
     
-    #[fail(display = "Unknown command type")]
+    #[error("Unknown command type")]
     UnexpectedCommandType,
 
-    #[fail(display = "Unknown engine type")]
+    #[error("Unknown engine type")]
     EngineErr
 
-}
-
-impl From<io::Error> for KvsError {
-    fn from(err: io::Error) -> KvsError {
-        KvsError::Io(err)
-    }
-}
-
-impl From<serde_json::Error> for KvsError {
-    fn from(err: serde_json::Error) -> KvsError {
-        KvsError::Serde(err)
-    }
-}
-
-impl From<sled::Error> for KvsError {
-    fn from(err: sled::Error) -> KvsError {
-        KvsError::Sled(err)
-    }
-}
-
-impl From<FromUtf8Error> for KvsError {
-    fn from(err: FromUtf8Error) -> Self {
-        KvsError::FromUTF8Err(err)
-    }
 }
